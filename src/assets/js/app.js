96d39028c151e9ffe210c9465ebecad8ea9b8bee
leoboyerbx <leo.boyer@outlook.com>
2020-02-13 15:31:05 +0100

    Nav refactor

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,44 +3,18 @@ import Nav from './modules/nav'
 
 $(document).ready(function() {
     const slidingDuration = parseFloat($('.slide').css('transition-duration')) * 1000
-    const $parent = $('#page-wrapper')
+    const $parent = $('#page-wrapper')    
+    window.nav = new Nav('.slide', $parent, slidingDuration)
 
-    
-    let nav = new Nav('.slide', slidingDuration)
-    let animating = false;
-
-    function scrollHandler (ev) {
-        const direction = ev.originalEvent.deltaY
-        if (nav.currentSlide === 0
-            || ($parent.scrollTop() === 0 && direction < 0)
-            || ($parent.scrollTop() + $parent.height() >= $parent.get(0).scrollHeight && direction > 0)
-            ) {
-            ev.preventDefault()
-            if (!animating) {
-                animating = true
-                if (direction > 0) {
-                    nav.next()
-                    $('#incite-wrapper').removeClass('visible')
-                } else {
-                    nav.prev()
-                }
-    
-                setTimeout(() => animating = false, slidingDuration)
-            }
-
-        }
-    }
 
 
     //incite
-    setTimeout(() => $('#incite-wrapper').addClass('visible'), 10)
     $('#incite, #readmore').click(function () {
         nav.next()
-        $('#incite-wrapper').removeClass('visible')
     })
 
     // $('#page-wrapper').get(0).addEventListener('wheel', scrollHandler)
-    $('#page-wrapper').on('wheel', scrollHandler)
+    $('#page-wrapper').on('wheel', nav.scrollHandler)
 
 
 })
--- src/assets/js/modules/nav.js
+++ src/assets/js/modules/nav.js
@@ -1,13 +1,26 @@
 import $ from 'jquery'
 
 export default class Nav {
-    constructor (slides, updateDelay = 0) {
+    constructor (slides, parent, updateDelay = 0) {
         this.slides = $(slides)
         this.numSlides = this.slides.length
         this.currentSlide = 0
+        this.parent = parent
         this.updateDelay = updateDelay
+        this.animating = false
+        this.scrollHandler = this.scrollHandler.bind(this)
+
+
+        this.incite = {
+            $: $('#incite-wrapper'),
+            show: () => this.incite.$.addClass('visible'),
+            showFirstTime: () => setTimeout(() => this.incite.$.addClass('delayVisible'), 10),
+            hide: () => this.incite.$.removeClass('visible', 'delayVisible')
+        }
+        this.incite.showFirstTime()
     }
     next () {
+        this.incite.hide()
         this.goTo(this.currentSlide + 1)
     }
     prev () {
@@ -20,5 +33,27 @@ export default class Nav {
             this.slides.get(index).classList.add('current')
             setTimeout(() => this.currentSlide = index, this.updateDelay)
         }
+    }
+    scrollHandler (ev) {
+        if (
+            ev.target.classList.contains('section-container')
+         || ev.target.classList.contains('section-container-wrapper')
+         || this.parent.scrollTop() > 0
+         ) {
+        } else {
+            const direction = ev.originalEvent.deltaY
+            ev.preventDefault()
+            if (!this.animating) {
+                this.animating = true
+                if (direction > 0) {
+                    this.next()
+                } else {
+                    this.prev()
+                }
+    
+                setTimeout(() => this.animating = false, this.slidingDuration)
+            }
+        }
+
     }
 }
