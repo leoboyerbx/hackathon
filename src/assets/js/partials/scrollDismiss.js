93062db77ef34471aad85e8b6d6415589e8aa64d
leoboyerbx <leo.boyer@outlook.com>
2020-02-14 17:40:12 +0100

    WIP on scroll and finished v1 of part header
    Co-authored-by: MatPic <mateo.piccarreta@gmail.com>

--- src/assets/js/partials/scrollDismiss.js
+++ src/assets/js/partials/scrollDismiss.js
@@ -2,6 +2,17 @@ import ScrollMoov from "../modules/ScrollMoov"
 
 export default function scrollDismiss (sectionsSelector) {
     document.querySelectorAll(sectionsSelector).forEach(section => {
-        
+        const titleWrapper = section.querySelector('.part-title-wrapper')
+        if (titleWrapper) {
+            new ScrollMoov(titleWrapper, {
+                opacity: 1
+            }, {
+                opacity: 0
+            }, {
+                considerEndScroll: false,
+                startPoint: 0,
+                endPoint: 1000
+            })
+        }
     })
 }
