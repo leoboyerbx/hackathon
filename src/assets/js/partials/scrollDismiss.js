5d2c333e5fe94c808243cf3660581b1d5d5c5ab2
leoboyerbx <leo.boyer@outlook.com>
2020-02-15 09:54:24 +0100

    V1 du fondu lors du scroll fonctionne

--- src/assets/js/partials/scrollDismiss.js
+++ src/assets/js/partials/scrollDismiss.js
@@ -18,13 +18,16 @@ export default function scrollDismiss (sectionsSelector, parent) {
             })
         }
 
-        const backgroundNumber = section.dataset.relatedBackground ? document.querySelector(section.dataset.relatedBackground) : null
+        const backgroundNumber = section.dataset.relatedBackground ? document.querySelector(section.dataset.relatedBackground +' .background-title-wrapper') : null
+        console.log(section.dataset.relatedBackground)
         if (backgroundNumber) {
             console.log(backgroundNumber)
             new ScrollMoov(backgroundNumber, {
                 opacity: 1,
+                scale: '1.4',
             }, {
                 opacity: 0.5,
+                scale: '1',
             }, {
                 considerEndScroll: false,
                 startPoint: 0,
